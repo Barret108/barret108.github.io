048513346db668dbb264d638580565dcd77ad553
Barret108 <barretappiah@gmail.com>
2022-12-21 14:24:48 -0700

    d

--- main.js
+++ main.js
@@ -219,33 +219,36 @@ var country = [
   "Zimbabwe",
 ];
 
-var pick = 0;
-var check = 0;
-var correct = 0;
-input = document.getElementById("inp");
-board = document.getElementById("score");
+var pick = 0
+var check = 0
+var correct = 0
+input = document.getElementById("inp")
+board = document.getElementById('score')
 
-changeImg();
+changeImg()
 
 function changeImg() {
+
+
   // Change Country
   pick = country[Math.round(Math.random() * 217)];
-  console.log(pick);
-  document.getElementById("imgSlot").src = `img/${pick}.svg`;
+  console.log(pick.toUpperCase())
+  document.getElementById("imgSlot").src = `img/${pick.toUpperCase()}.svg`;
+  
 }
 
 function checkAns() {
-  check++;
-  if (inp.value == pick) {
-    console.log("Correct");
-    correct++;
+  check++
+  if(inp.value == pick) {
+    console.log("Correct")
+    correct++
   } else {
-    console.log(`Answer = ${pick}`);
+    console.log(`Answer = ${pick}`)
   }
-  changeImg();
-  inp.value = "";
+  changeImg()
+  inp.value = ''
 
-  board.innerHTML = `${correct} / ${check} / ${Math.round(
-    (correct / check) * 100
-  )}%`;
+  board.innerHTML = `${correct} / ${check} / ${Math.round((correct / check) * 100)}%`
 }
+
+
